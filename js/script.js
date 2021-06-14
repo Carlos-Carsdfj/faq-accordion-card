3fe884ba94a78e6504db635320c91d5eb5ed2a3b
carsdf <carsdfj@gmail.com>
2021-06-14 16:29:55 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// const link = document.querySelectorAll('.title-question')
+// const info = document.querySelector('.content-info')
+
+// // info.forEach(is=>{
+// //     is.classList.add('active')
+// // })
+
+
+
+// link.forEach( itemTitle => {
+    
+
+//     itemTitle.addEventListener("click",() => {
+
+//         itemTitle.document.querySelector('.content-info').classList.add('active')
+        
+    
+
+//     })
+
+    
+// })
